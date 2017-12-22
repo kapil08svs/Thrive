8cbe9c0f3a27d5babf004478e0c277a09b101926
Anirudh Chakravarthy <ani.chakravarthy95@gmail.com>
2017-12-22 22:31:16 +0530

    Added the necesary files in the application.scss folder for the bootstrap to work.

---
+++ website/app/controllers/website_indexpage_controller.rb
@@ -0,0 +1,4 @@
+class WebsiteIndexpageController < ApplicationController
+  def index
+  end
+end
--- website/config/routes.rb
+++ website/config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'website_indexpage/index'
+  root 'website_indexpage#index'
 end
